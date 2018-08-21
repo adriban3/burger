6d41ca213ae32d35e51fe2587354f6fa33d8e0e4
Andrew Driban <dribaan@habasitusa.net>
2018-08-21 12:52:30 -0400

    made some quick updates at lunch

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,22 @@
 var orm = require("../config/orm");
 
-//logic for calling orm functions and export here
+var burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
