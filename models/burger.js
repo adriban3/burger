cf627d448e0f39d984e223a07851d1f41d8c2024
Andrew <andrewmd4774@gmail.com>
2018-08-22 22:27:04 -0400

    fixing it

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ var orm = require("../config/orm");
 
 var burger = {
   all: function(cb) {
-    orm.selectAll("burgers_db.burgers", function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.insertOne("burgers_db.burgers", cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.updateOne("burgers_db.burgers", objColVals, condition, function(res) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
