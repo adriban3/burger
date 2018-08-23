2362e5f0e51f2ad74c23e97a8049fde4a0eeb995
Andrew <andrewmd4774@gmail.com>
2018-08-22 22:08:00 -0400

    logging jaws string

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
+  console.log(process.env.JAWSDB_URL);
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 
